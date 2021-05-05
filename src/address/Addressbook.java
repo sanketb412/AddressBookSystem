d7a19fac99147dba2bf10529d3e95967e9391bb9
sanketb412 <sanketbagde38@gmail.com>
2021-05-05 11:58:17 +0530

    Added: code for editing the existing Name

--- src/address/Addressbook.java
+++ src/address/Addressbook.java
@@ -30,14 +30,15 @@ public class Addressbook {
 		sc.nextLine();
 		System.out.println("Enter the Contact Number");
 		String Contact_Number = sc.nextLine();
-		System.out.println("Enter the Email address\n");
+		System.out.println("Enter the Email address");
 		String eMail = sc.nextLine();
 
 		Addressbook entry = new Addressbook(First_Name, Last_Name, Address, City, State, zip, Contact_Number, eMail);
 		entry.addressBook();
 	}
 
-	public Addressbook(String First_Name, String Last_Name, String Address, String City, String State, int zip, String Contact_Number, String eMail) {
+	public Addressbook(String First_Name, String Last_Name, String Address, String City, String State, int zip,
+			String Contact_Number, String eMail) {
 		this.First_Name = First_Name;
 		this.Last_Name = Last_Name;
 		this.Address = Address;
@@ -53,7 +54,7 @@ public class Addressbook {
 	}
 
 	public void addressBook() {
-		System.out.println("First Name: " + First_Name);
+		System.out.println("\nFirst Name: " + First_Name);
 		System.out.println("Last Name: " + Last_Name);
 		System.out.println("Address: " + Address);
 		System.out.println("City: " + City);
@@ -63,12 +64,40 @@ public class Addressbook {
 		System.out.println("Email Address: " + eMail);
 	}
 
+	public void editPerson() {
+		System.out.println("\nCross Check the details. if you want to edit Type '1' for Yes or '0' for No ");
+		Scanner sc = new Scanner(System.in);
+		int YN = sc.nextInt();
+		if (YN == 1) {
+			Scanner sc1 = new Scanner(System.in);
+			System.out.println("\nEnter the choice: What you Want to edit");
+			System.out.println("1:- First_Name \n2:- Last_Name");
+			int edit = sc1.nextInt();
+			switch (edit) {
+			case 1:
+				System.out.println("Enter the New Name What you want to Edit");
+				Scanner sc2 = new Scanner(System.in);
+				String FN = sc2.nextLine();
+				System.out.println("The New Name Entered is: " + FN);
+				break;
+			case 2:
+				System.out.println("Enter the New Name What you want to Edit");
+				Scanner sc3 = new Scanner(System.in);
+				String LN = sc3.nextLine();
+				System.out.println("The New Nmae Entered is: " + LN);
+				break;
+			}
+		} else {
+			System.out.println("###..Nothing to edit..###");
+		}
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("..####..Welcome to Address Book Programm..####..");
 		Addressbook register = new Addressbook();
 		register.addContactDetails();
-
+		register.editPerson();
 	}
 
 }
